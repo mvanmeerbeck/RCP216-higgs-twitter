a5d292f20862c075b3c0009ddf222f1c383637cd
Maxime.Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2018-12-15 23:06:24 +0100

    refac

--- src/main/scala/HiggsTwitter.scala
+++ src/main/scala/HiggsTwitter.scala
@@ -1,12 +1,13 @@
 import java.io.File
+
 import org.apache.spark.graphx.{Graph, GraphLoader, VertexId}
 import org.apache.spark.sql.SparkSession
+
 import scala.reflect.io.Directory
 
 object HiggsTwitter {
     private val appName = "HiggsTwitter"
     private val rootPath = scala.util.Properties.envOrElse("HOME", "~/" + appName)
-    private val socialNetworkDegreeDistributionDirectory = new Directory(new File(rootPath + "/SocialNetwork/DegreeDistribution"))
 
     def main(args: Array[String]) {
         val spark = SparkSession
@@ -14,16 +15,11 @@ object HiggsTwitter {
             .master("local[*]")
             .appName(appName)
             .getOrCreate()
-        println(this.getClass.getName)
-        socialNetworkDegreeDistributionDirectory.deleteRecursively()
 
         // Social Network Graph
         val socialNetwork: Graph[Int, Int] = GraphLoader
             .edgeListFile(spark.sparkContext, args(0))
             .cache()
-
-        println(socialNetwork.edges.count())
-        println(socialNetwork.vertices.count())
 
         // Connected components
         val connectedComponents: Graph[VertexId, Int] = socialNetwork
@@ -42,26 +38,32 @@ object HiggsTwitter {
         //componentCounts.take(10).foreach(println)
 
         // Degrees
-        val socialNetworkDegrees = socialNetwork
+        exportDegreeDistribution(
+            socialNetwork,
+            new Directory(new File(rootPath + "/SocialNetwork/DegreeDistribution"))
+        )
+
+        spark.stop()
+    }
+
+    def exportDegreeDistribution(graph: Graph[Int, Int], directory: Directory): Unit = {
+        val graphDegrees = graph
             .degrees
             .cache()
 
-        val userCount = socialNetwork.vertices.count()
+        val verticesCount = graph.vertices.count()
 
-        val socialNetworkDegreeDistribution = socialNetworkDegrees
+        val graphDegreeDistribution = graphDegrees
             .map(degree => (degree._2, 1F))
             .reduceByKey(_ + _)
-            .map(degree => (degree._1, degree._2 / userCount))
-            .sortBy(_._1, false)
+            .map(degree => (degree._1, degree._2 / verticesCount))
 
-        socialNetworkDegreeDistribution
+        directory.deleteRecursively()
+
+        graphDegreeDistribution
             .repartition(1)
             .map(data => formatCsv(data))
-            .saveAsTextFile(socialNetworkDegreeDistributionDirectory.path)
-
-        println(socialNetworkDegrees.map(_._2).stats())
-
-        spark.stop()
+            .saveAsTextFile(directory.path)
     }
 
     def formatCsv(data: Product): String = {
